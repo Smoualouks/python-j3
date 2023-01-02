b873c7f30465b77179eed1f127cfd29e29f960ce
OMARI <106803565+Smoualouks@users.noreply.github.com>
2023-01-02 22:00:06 +0100

    Update tp4.py
    to be taked please

--- tp4/tp4.py
+++ tp4/tp4.py
@@ -12,17 +12,20 @@ class PageMaker:
      def __init__(self, entry_file = "deniro.csv"):
          self.__entry_file = entry_file
          
+         
      # guetteur :
 
-     def get_entry_file(self, __entry_file):
-       return self.__entry_file
+     def get_entry_file(self):
+         
+         return self.__entry_file
+       
         
 
     #la méthdode generate-html pour générer les pages htmls pour chaque film:    
 
      def generate_html(self, file_destination):
         
-        file_path = self.__entry_file()
+        file_path = self.get_entry_file()
     
         with open(file_path, 'r') as f:
              # je lis le contenu du fichier csv.
@@ -38,8 +41,8 @@ class PageMaker:
                 newcontent = content.replace("[Title]", "r[2]" )
                 newcontent = content.replace("[Year]", "r[0]" )
                 newcontent = content.replace("[Score]", "r[1]" )
-
-                f3 = open("file_destination"+ "/" + r[2][2:-1],"w")
+                
+                f3 = open(file_destination + "/" + r[2][2:-1],"w")
                 f3.write(newcontent)
                 f3.close()
 
