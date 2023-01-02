73328b9f77700c020385139864786688f0e564fc
OMARI <106803565+Smoualouks@users.noreply.github.com>
2023-01-02 20:47:29 +0100

    Update tp4.py
    tp4 my version ++

--- tp4/tp4.py
+++ tp4/tp4.py
@@ -1,4 +1,4 @@
-import os
+import csv, os
 
 # définition de la classe PageMaker:
 
@@ -9,14 +9,14 @@ class PageMaker:
      __entry_file = ""
 
     # définition du contsructeur qui prend une entree csv au format de deniro:
-    def __init__(self, entry_file= "deniro.csv"):
-        self.__entry_file = entry_file
+     def __init__(self, entry_file):
+            self.__entry_file = entry_file
          
         
 
     #la méthdode generate-html pour générer les pages htmls pour chaque film:    
 
-    def generate_html(self, file_destination):
+     def generate_html(self, file_destination):
         
         file_path = self.__entry_file
 
@@ -27,20 +27,31 @@ class PageMaker:
             # j'invoque la méthode next() pour sauter la première ligne contenant le nom des colonnes.
              next(rows)
 
-            for r in rows:
-                with open("template.html", "w") as f2:
-                    content = f2.read()
-                    f2.close()
-                    newcontent = content.teplace("[Title]", "r[2]" )
-                    newcontent = content.teplace("[Year]", "r[0]" )
-                    newcontent = content.teplace("[Score]", "r[1]" )
+             for r in rows:
+                f2 = open("template.html", "w")
+                content = f2.read()
+                f2.close()
+                newcontent = content.teplace("[Title]", "r[2]" )
+                newcontent = content.teplace("[Year]", "r[0]" )
+                newcontent = content.teplace("[Score]", "r[1]" )
 
-                    f3 = open("file_destination"+ "/" + r[2][2:-1],"w")
-                    f3.write(newcontent)
-                    f3.close()
+                f3 = open("file_destination"+ "/" + r[2][2:-1],"w")
+                f3.write(newcontent)
+                f3.close()
+
+
+              
+pm = PageMaker("deniro.csv")
+pm.generate_html("/tmp")
 
 
 
+
+                
+            
+       
+
+       
 
 
               
